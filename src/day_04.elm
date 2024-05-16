8cc63cf5274298a8c416e83e0e165736cd9226a5
Selim Dincer <wowselim@live.de>
2024-05-16 20:19:10 +0300

    add day 4

---
+++ src/day_04.elm
@@ -0,0 +1,26 @@
+list =
+  [1, 3, 4]
+
+list
+
+list =
+  [1, 5.6]
+
+list
+
+tuple =
+  (1, 'd', "dog")
+
+tuple
+
+record =
+  { name = "Selim", age = 1337, isAlive = True }
+
+record
+
+record.name
+
+copyOfRecord =
+  { record | age = 4902 }
+
+copyOfRecord
